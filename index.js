48f76a8480b17d2c629a7ee5edf25b53a6fe75f8
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-02 20:13:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string){
+string.toLowerCase() === string {
+    return "I can't hear you!"
+  }
+}
+
